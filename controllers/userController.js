e3105deb43c5ecd035980e48732c3c71f2f4cf9a
wadhass <ahmedhassansumu@gmail.com>
2025-03-28 10:00:45 +0200

    ok

--- controllers/userController.js
+++ controllers/userController.js
@@ -57,7 +57,7 @@ export const userEnrolledCourses = async (req, res) => {
 export const purchaseCourse = async (req, res) => {
     try {
         const { courseId } = req.body;
-        const origin = req.headers.origin || "https://server-added.onrender.com"; // fallback in case origin is missing
+        const origin = req.headers.origin || "http://localhost:3000"; // fallback in case origin is missing
         const clerkUserId = req.auth.userId; // Clerk user ID
 // http://localhost:3000
         // Find the user by Clerk ID
